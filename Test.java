84751a7709f299aa35da367f9d20f68e7ea04af6
Vijay703217 <155128927+Vijay703217@users.noreply.github.com>
2023-12-29 15:22:56 +0530

    give no is positive or negitive or zero

---
+++ Test.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Test
+
+{
+	public static void main(String[]args)
+	{
+		Scanner s=new Scanner(System.in);
+	
+	int a;
+	System.out.println("enter a number");
+     a=s.nextInt();
+       
+	 
+		 if(a>0)
+		{
+		 System.out.println("positive");
+		
+ 		}
+        else
+			if(a<0)
+		      {
+
+            	 System.out.println("negitive");
+	           }
+            else
+	        	{
+                  System.out.println("zero");
+				}}}
+
