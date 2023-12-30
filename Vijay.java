f8335579650c0af8e73c4ea8f79038a51dea00b5
Vijay703217 <155128927+Vijay703217@users.noreply.github.com>
2023-12-30 10:55:36 +0530

    Ternary operator
     finding biggest no using  Ternary operator

---
+++ Vijay.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ class Vijay
+{
+public static void main(String [] args)
+{
+ Scanner s=new Scanner(System.in);
+int a,b;
+a=s.nextInt();
+b=s.nextInt();
+int r=(a>b)?a:b;
+System.out.println(r);
+}
+}
